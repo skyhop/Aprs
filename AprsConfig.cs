367ff27d6849f653036a0cbf0b606972ab5abb72
Corstian Boerman <mail@corstianboerman.com>
2017-10-01 22:16:41 +0200

    The APRS client compiles on .NET Standard 2 now.
    Please note it'd be nice to have all dependencies published on NuGet before pushing this one out. Also, please don't forget to test and rewrite some documentation (mainly for the configuration stuff)

---
+++ AprsConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Boerman.Aeronautics.AprsClient
+{
+    public static class AprsConfig
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        static AprsConfig() {
+			Configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json").Build();
+        }
+
+        public static string Uri => Configuration["aprsClient:uri"] ?? "noam.aprs2.net";
+        public static int Port => Convert.ToInt32(Configuration["aprsClient:port"] ?? "14580");
+        public static string Callsign => Configuration["aprsClient:callsign"] ?? "0";
+        public static string Password => Configuration["aprsClient:password"] ?? "-1";
+        public static string Filter => Configuration["aprsClient:filter"] ?? "t/poimqstunw";
+        public static bool UseOgnAdditives => Convert.ToBoolean(Configuration["aprsClient:useOgnAdditives"] ?? "true");
+    }
+}
